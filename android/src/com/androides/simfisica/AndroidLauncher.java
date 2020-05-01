cf2da9b9df3503a3ef342ef124daccf06db501a5
albgv1402 <albertogarv1402@gmail.com>
2020-05-01 12:54:25 -0500

    Created GDX project

---
+++ android/src/com/androides/simfisica/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.androides.simfisica;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.androides.simfisica.SimuladorFisica;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SimuladorFisica(), config);
+	}
+}
