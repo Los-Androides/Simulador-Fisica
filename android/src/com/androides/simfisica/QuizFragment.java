503c3b8f66159dae961f02bc7121a647ec7242ae
ChristopherParga <38229095+ChristopherParga@users.noreply.github.com>
2020-05-31 18:55:23 -0500

    Funcionalidad Quiz
    Funciones de los botones

--- android/src/com/androides/simfisica/QuizFragment.java
+++ android/src/com/androides/simfisica/QuizFragment.java
@@ -1,10 +1,14 @@
 package com.androides.simfisica;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -21,14 +25,22 @@ public class QuizFragment extends Fragment {
     public int mRadioButtonChoice;
     private TextView textView;
     private Button check;
-    private Button prev;
+    private Button reset;
     private Button next;
+    private Button mostrar;
+    private int tries = 0;
+    private TextView puntuacion;
+    private TextView nivel;
+    private TextView reto;
+    private int numPreguntas;
+    private EditText respuesta;
 
 
 
     public QuizFragment() {
         // Required empty public constructor
         Preguntas = new Preguntas();
+        numPreguntas = Preguntas.getSize();
     }
 
 
@@ -36,51 +48,94 @@ public class QuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         final View rootView = inflater.inflate(R.layout.activity_quiz,container,false);
         textView = rootView.findViewById(R.id.Pregunta);
+        respuesta = rootView.findViewById(R.id.respuesta);
+        mostrar = rootView.findViewById(R.id.button4);
         textView.setText(Preguntas.getPregunta(numPregunta));
+        reto = rootView.findViewById(R.id.Reto);
+        reto.setText("Reto " + Integer.toString(numPregunta + 1) + " de " + Integer.toString(numPreguntas));
+        puntuacion = rootView.findViewById(R.id.Puntuacion);
+        puntuacion.setText("Puntuacion: " + Integer.toString(score));
         ImageView Imagen = rootView.findViewById(R.id.imageView);
         Imagen.setImageBitmap(Preguntas.getImage(numPregunta));
-        //prev = rootView.findViewById(R.id.Prev);
+        mostrar.setEnabled(false);
+        mostrar.setVisibility(rootView.INVISIBLE);
         next = rootView.findViewById(R.id.Next);
         check = rootView.findViewById(R.id.checkAns);
-        check.setEnabled(false);
+        next.setEnabled(false);
+        reset = rootView.findViewById(R.id.reset);
 
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetQuiz(rootView);
+            }
+        });
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String answer = respuesta.getText().toString().trim();
+                if(answer == Preguntas.getRespuesta(numPregunta).trim()){
+                    Toast.makeText(rootView.getContext(), "CORRECTO!",Toast.LENGTH_SHORT).show();
+                    score = score + 2;
+                    puntuacion.setText("Puntuacion: " + Integer.toString(score));
+                    next.setEnabled(true);
+                }else{
+                    Toast.makeText(rootView.getContext(), "Intentalo de nuevo!",Toast.LENGTH_SHORT).show();
+                    tries++;
+                    if(tries == 3){
+                        mostrar.setVisibility(rootView.VISIBLE);
+                        mostrar.setEnabled(true);
+                        mostrar.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(rootView.getContext(), "La respuesta es: " +
+                                        Preguntas.getRespuesta(numPregunta),Toast.LENGTH_SHORT).show();
+                                score--;
+                                puntuacion.setText("Puntuacion: " + Integer.toString(score));
+                                next.setEnabled(true);
+                            }
+                        });
+                    }
+                }
+            }
+        });
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                numPregunta++;
+                textView.setText(Preguntas.getPregunta(numPregunta));
+                puntuacion.setText("Puntuacion: " + Integer.toString(score));
                 sigPregunta(rootView);
             }
         });
 
-        //prev.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View v) {
-              //  prevPregunta();
-            //}
-        //});
-
-
-
-
-
-
         return rootView;
     }
+
+
+
 
     private void sigPregunta(View rootView){
         numPregunta++;
         textView.setText(Preguntas.getPregunta(numPregunta));
-        prev.setEnabled(true);
-        check.setVisibility(rootView.GONE);
-        check.setEnabled(false);
+        reto.setText("Reto " + Integer.toString(numPregunta + 1) + " de " + Integer.toString(numPreguntas));
+        next.setEnabled(false);
+        mostrar.setEnabled(false);
+        mostrar.setVisibility(rootView.INVISIBLE);
+        respuesta.setText("");
     }
 
-    private void prevPregunta(){
-        numPregunta--;
+    private void resetQuiz(View rootView){
+        numPregunta = 0;
+        score = 0;
         textView.setText(Preguntas.getPregunta(numPregunta));
-        if(numPregunta == 0){
-            prev.setEnabled(false);
-        }
+        puntuacion.setText("Puntuacion: " + Integer.toString(score));
+        next.setEnabled(false);
+        respuesta.setText("");
+        mostrar.setEnabled(false);
+        mostrar.setVisibility(rootView.INVISIBLE);
+        reto.setText("Reto " + Integer.toString(numPregunta + 1) + " de " + Integer.toString(numPreguntas));
     }
 }
 
